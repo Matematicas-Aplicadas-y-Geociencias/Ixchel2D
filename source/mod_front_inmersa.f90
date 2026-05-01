ba915b128deb0b2c24927850e84259dadffad236
carlos-cajas <carlos.cajas@enesmerida.unam.mx>
2026-05-01 07:29:48 -0600

    Se agregan los valores de au y av en la definición del cuerpo inmerso desde la entrada de datos en el mod_front_inmersa

--- source/mod_front_inmersa.f90
+++ source/mod_front_inmersa.f90
@@ -128,11 +128,13 @@ contains
        fuente_con_t(ii,jj) = real(fue_con,DBL)
        fuente_lin_t(ii,jj) = real(fue_lin,DBL)
        !
-       fuente_con_u(ii-1,jj) = real(fue_con,DBL)
-       fuente_lin_u(ii-1,jj) = real(fue_lin,DBL)
+       fuente_con_u(ii,jj) = real(fue_con,DBL)
+       fuente_lin_u(ii,jj) = real(fue_lin,DBL)
+       au(ii,jj)           = real(fue_lin,DBL) 
        !
-       fuente_con_v(ii,jj-1) = real(fue_con,DBL)
-       fuente_lin_v(ii,jj-1) = real(fue_lin,DBL)
+       fuente_con_v(ii,jj) = real(fue_con,DBL)
+       fuente_lin_v(ii,jj) = real(fue_lin,DBL)
+       av(ii,jj)           = real(fue_lin,DBL)
        !
     end do
     !
